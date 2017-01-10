f7a7faead41c9faa7c9745882bd09fece3f1240c
tubesoft <inotazo@hotmail.co,jp>
2017-01-09 22:20:43 -0500

    Initial Commit

---
+++ SampleTapLoopTests/SampleTapLoopTests.swift
@@ -0,0 +1,36 @@
+//
+//  SampleTapLoopTests.swift
+//  SampleTapLoopTests
+//
+//  Created by Takatomo INOUE on 2017/01/09.
+//  Copyright © 2017年 Takatomo INOUE. All rights reserved.
+//
+
+import XCTest
+@testable import SampleTapLoop
+
+class SampleTapLoopTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
